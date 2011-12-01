23912283cf56ffe373225946ef5b4596a62ec0a7
mattn <mattn.jp@gmail.com>
2011-12-01 19:44:10 +0900

    imprements.

--- example/main.go
+++ example/main.go
@@ -4,11 +4,39 @@ import "github.com/mattn/go-uv"
 
 func main() {
 	tcp, _ := uv.TcpInit()
-	tcp.Bind("0.0.0.0", 8888)
+	err := tcp.Bind("0.0.0.0", 8888)
+	if err != nil {
+		panic(err)
+	}
 	tcp.Listen(10, func(status int) {
 		client, _ := tcp.Accept()
+		println("server: accept")
 		client.ReadStart(func(data []byte) {
-			println("foo")
+			println("client: read", string(data))
+			client.Write(data, func(status int) {
+				println("client: written")
+			})
+			client.Close(func() {
+				println("client: closed")
+			})
+			tcp.Close(func() {
+				println("server: closed")
+			})
 		})
 	})
+
+	/*
+	go func() {
+		time.Sleep(1e9)
+		tcp, _ := uv.TcpInit()
+		tcp.Connect("0.0.0.0", 8888, func(status int) {
+			println(status)
+			tcp.Write([]byte("Hello World"), func(status int) {
+				println("sender: sent!", status)
+			})
+		})
+	}()
+	*/
+
+	uv.Run()
 }
--- uv.go
+++ uv.go
@@ -1,15 +1,28 @@
 package uv
 
 /*
+#include <stdio.h>
 #include <stdlib.h>
 #include <uv/uv.h>
 
 extern void __uv_connect_cb(void* p, int status);
-static void _uv_connect_cb(uv_stream_t* server, int status) {
-	__uv_connect_cb(server, status);
+static void _uv_connect_cb(uv_connect_t* req, int status) {
+	__uv_connect_cb(req->handle->data, status);
 }
 
+extern void __uv_connection_cb(void* p, int status);
+static void _uv_connection_cb(uv_stream_t* server, int status) {
+	__uv_connection_cb(server->data, status);
+}
+
+extern void __uv_read_cb(void* p, int nread, void* buf);
 static void _uv_read_cb(uv_stream_t* stream, ssize_t nread, uv_buf_t buf) {
+	__uv_read_cb(stream->data, nread, buf.base);
+}
+
+extern void __uv_write_cb(void* p, int status);
+static void _uv_write_cb(uv_write_t* req, int status) {
+	__uv_write_cb(req->handle->data, status);
 }
 
 static uv_buf_t _uv_alloc_cb(uv_handle_t* handle, size_t suggested_size) {
@@ -18,40 +31,58 @@ static uv_buf_t _uv_alloc_cb(uv_handle_t* handle, size_t suggested_size) {
     return uv_buf_init(buf, suggested_size);
 }
 
+extern void __uv_close_cb(void* p);
+static void _uv_close_cb(uv_handle_t* handle) {
+	__uv_close_cb(handle->data);
+}
+
 static int _uv_listen(uv_stream_t* stream, int backlog) {
-	return uv_listen(stream, backlog, _uv_connect_cb);
+	return uv_listen(stream, backlog, _uv_connection_cb);
+}
+
+static int _uv_tcp_connect(uv_connect_t* req, uv_tcp_t* handle, struct sockaddr_in address) {
+	return uv_tcp_connect(req, handle, address, _uv_connect_cb);
 }
 
 static int _uv_read_start(uv_stream_t* stream) {
 	return uv_read_start(stream, _uv_alloc_cb, _uv_read_cb);
 }
 
-static uv_stream_t* _uv_tcp_to_string(uv_tcp_t* tcp) {
+static int _uv_write(uv_write_t* req, uv_stream_t* handle, uv_buf_t bufs[], int bufcnt) {
+	return uv_write(req, handle, bufs, bufcnt, _uv_write_cb);
+}
+
+static void _uv_close(uv_handle_t* handle) {
+	uv_close(handle, _uv_close_cb);
+}
+
+static uv_stream_t* _uv_tcp_to_stream(uv_tcp_t* tcp) {
 	return (uv_stream_t*) tcp;
 }
 
-#cgo CFLAGS: -static
-#cgo LDFLAGS: -static -luv -lws2_32
+static uv_handle_t* _uv_tcp_to_handle(uv_tcp_t* tcp) {
+	return (uv_handle_t*) tcp;
+}
+
+#cgo darwin LDFLAGS: -luv
+#cgo linux LDFLAGS: -luv
+#cgo windows LDFLAGS: -luv.dll -lws2_32
 */
 import "C"
 import "errors"
 import "unsafe"
 
 type Tcp struct {
-	t C.uv_tcp_t
+	t *C.uv_tcp_t
 }
 
-type Stream struct {
-	s C.uv_stream_t
-}
-
-type Client struct {
-	c C.uv_stream_t
-}
-
-type callback_info struct {
+type CallbackInfo struct {
+	data string
+	connection_cb func(int)
 	connect_cb func(int)
 	read_cb func([]byte)
+	write_cb func(int)
+	close_cb func()
 }
 
 func TcpInit() (tcp *Tcp, err error) {
@@ -62,14 +93,28 @@ func TcpInit() (tcp *Tcp, err error) {
 		e := C.uv_last_error(C.uv_default_loop())
 		return nil, errors.New(C.GoString(C.uv_strerror(e)))
 	}
-	t.data = unsafe.Pointer(&callback_info{})
-	return &Tcp {t}, nil
+	t.data = unsafe.Pointer(&CallbackInfo{})
+	return &Tcp {&t}, nil
 }
 
 func (tcp *Tcp) Bind(host string, port uint16) (err error) {
 	phost := C.CString(host)
 	defer C.free(unsafe.Pointer(phost))
-	r := C.uv_tcp_bind(&tcp.t, C.uv_ip4_addr(phost, C.int(port)));
+	r := C.uv_tcp_bind(tcp.t, C.uv_ip4_addr(phost, C.int(port)))
+	if r != 0 {
+		e := C.uv_last_error(C.uv_default_loop())
+		return errors.New(C.GoString(C.uv_strerror(e)))
+	}
+	return nil
+}
+
+func (tcp *Tcp) Connect(host string, port uint16, cb func(int)) (err error) {
+	cbi := (*CallbackInfo)(tcp.t.data)
+	cbi.connect_cb = cb
+	phost := C.CString(host)
+	defer C.free(unsafe.Pointer(phost))
+    var req C.uv_connect_t
+	r := C._uv_tcp_connect(&req, tcp.t, C.uv_ip4_addr(phost, C.int(port)))
 	if r != 0 {
 		e := C.uv_last_error(C.uv_default_loop())
 		return errors.New(C.GoString(C.uv_strerror(e)))
@@ -78,9 +123,9 @@ func (tcp *Tcp) Bind(host string, port uint16) (err error) {
 }
 
 func (tcp *Tcp) Listen(backlog int, cb func(int)) (err error) {
-	cbi := (*callback_info)(tcp.t.data)
-	cbi.connect_cb = cb
-	r := C._uv_listen(C._uv_tcp_to_string(&tcp.t), C.int(backlog))
+	cbi := (*CallbackInfo)(tcp.t.data)
+	cbi.connection_cb = cb
+	r := C._uv_listen(C._uv_tcp_to_stream(tcp.t), C.int(backlog))
 	if r != 0 {
 		e := C.uv_last_error(C.uv_default_loop())
 		return errors.New(C.GoString(C.uv_strerror(e)))
@@ -88,20 +133,23 @@ func (tcp *Tcp) Listen(backlog int, cb func(int)) (err error) {
 	return nil
 }
 
-func (tcp *Tcp) Accept() (client *Client, err error) {
-	var c C.uv_stream_t
-	r := C.uv_accept(C._uv_tcp_to_string(&tcp.t), &c)
+func (tcp *Tcp) Accept() (client *Tcp, err error) {
+	c, err := TcpInit()
+	if err != nil {
+		return nil, err
+	}
+	r := C.uv_accept(C._uv_tcp_to_stream(tcp.t), C._uv_tcp_to_stream(c.t))
 	if r != 0 {
 		e := C.uv_last_error(C.uv_default_loop())
 		return nil, errors.New(C.GoString(C.uv_strerror(e)))
 	}
-	return &Client{c}, nil
+	return &Tcp{c.t}, nil
 }
 
-func (client *Client) ReadStart(cb func([]byte)) (err error) {
-	cbi := (*callback_info)(client.c.data)
+func (tcp *Tcp) ReadStart(cb func([]byte)) (err error) {
+	cbi := (*CallbackInfo)(tcp.t.data)
 	cbi.read_cb = cb
-	r := C._uv_read_start(&client.c)
+	r := C._uv_read_start(C._uv_tcp_to_stream(tcp.t))
 	if r != 0 {
 		e := C.uv_last_error(C.uv_default_loop())
 		return errors.New(C.GoString(C.uv_strerror(e)))
@@ -109,8 +157,74 @@ func (client *Client) ReadStart(cb func([]byte)) (err error) {
 	return nil
 }
 
+func (tcp *Tcp) ReadStop() (err error) {
+	r := C.uv_read_stop(C._uv_tcp_to_stream(tcp.t))
+	if r != 0 {
+		e := C.uv_last_error(C.uv_default_loop())
+		return errors.New(C.GoString(C.uv_strerror(e)))
+	}
+	return nil
+}
+
+func (tcp *Tcp) Write(b []byte, cb func(int)) (err error) {
+	cbi := (*CallbackInfo)(tcp.t.data)
+	cbi.write_cb = cb
+    var req C.uv_write_t
+	buf := C.uv_buf_init((*C.char)(unsafe.Pointer(&b[0])), C.size_t(len(b)))
+	r := C._uv_write(&req, C._uv_tcp_to_stream(tcp.t), &buf, 1)
+	if r != 0 {
+		e := C.uv_last_error(C.uv_default_loop())
+		return errors.New(C.GoString(C.uv_strerror(e)))
+	}
+	return nil
+}
+
+func (tcp *Tcp) Close(cb func()) {
+	cbi := (*CallbackInfo)(tcp.t.data)
+	cbi.close_cb = cb
+	C._uv_close(C._uv_tcp_to_handle(tcp.t))
+}
+
 //export __uv_connect_cb
 func __uv_connect_cb(p unsafe.Pointer, status int) {
-	cbi := (*callback_info)(p)
-	cbi.connect_cb(status)
+	cbi := (*CallbackInfo)(p)
+	if cbi.connect_cb != nil {
+		cbi.connect_cb(status)
+	}
+}
+
+//export __uv_connection_cb
+func __uv_connection_cb(p unsafe.Pointer, status int) {
+	cbi := (*CallbackInfo)(p)
+	if cbi.connection_cb != nil {
+		cbi.connection_cb(status)
+	}
+}
+
+//export __uv_read_cb
+func __uv_read_cb(p unsafe.Pointer, nread int, buf unsafe.Pointer) {
+	cbi := (*CallbackInfo)(p)
+	if cbi.read_cb != nil {
+		cbi.read_cb((*[1 << 30]byte)(unsafe.Pointer(buf))[0:nread])
+	}
+}
+
+//export __uv_write_cb
+func __uv_write_cb(p unsafe.Pointer, status int) {
+	cbi := (*CallbackInfo)(p)
+	if cbi.write_cb != nil {
+		cbi.write_cb(status)
+	}
+}
+
+//export __uv_close_cb
+func __uv_close_cb(p unsafe.Pointer) {
+	cbi := (*CallbackInfo)(p)
+	if cbi.close_cb != nil {
+		cbi.close_cb()
+	}
+}
+
+func Run() {
+    C.uv_run(C.uv_default_loop())
 }
